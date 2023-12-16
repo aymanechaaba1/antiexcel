c5d487818afe652fc730cbc12c77beb7b8539588
Aymane Chaaba <aymanechaaba1@gmail.com>
2023-12-16 11:13:02 +0100

    fix hydration error

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,18 +1,27 @@
 'use client';
+import { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 
 function Video() {
+  const [showVideo, setShowVideo] = useState(false);
+
+  useEffect(() => {
+    setShowVideo(true);
+  }, [setShowVideo]);
+
   return (
-    <div className="max-w-3xl mx-auto my-5 border rounded-lg shadow-md p-2">
-      <ReactPlayer
-        url={`https://firebasestorage.googleapis.com/v0/b/school-manager-e26b7.appspot.com/o/edited-demo-antiexcel.mp4?alt=media&token=dfa44346-9bf5-4123-9072-f9cdda2a50a4`}
-        loop={true}
-        playing={true}
-        muted={true}
-        width={`100%`}
-        height={`auto`}
-      />
-    </div>
+    showVideo && (
+      <div className="max-w-3xl mx-auto my-5 border rounded-lg shadow-md p-2">
+        <ReactPlayer
+          url={`https://firebasestorage.googleapis.com/v0/b/school-manager-e26b7.appspot.com/o/edited-demo-antiexcel.mp4?alt=media&token=dfa44346-9bf5-4123-9072-f9cdda2a50a4`}
+          loop={true}
+          playing={true}
+          muted={true}
+          width={`100%`}
+          height={`auto`}
+        />
+      </div>
+    )
   );
 }
 
