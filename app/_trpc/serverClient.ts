9c04ed6b6fe38f440d296dadb624dfa7b039cf96
Aymane Chaaba <aymanechaaba1@gmail.com>
2023-12-17 12:01:22 +0100

    fix trpc

--- app/_trpc/serverClient.ts
+++ app/_trpc/serverClient.ts
@@ -1,10 +1,11 @@
+import { getUrl } from '@/lib/utils';
 import { appRouter } from '@/server';
 import { httpBatchLink } from '@trpc/client';
 
 export const serverClient = appRouter.createCaller({
   links: [
     httpBatchLink({
-      url: `http://localhost:443/api/trpc`,
+      url: `${getUrl()}/api/trpc`,
     }),
   ],
 });
