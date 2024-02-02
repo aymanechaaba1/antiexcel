95f8d9c2d9d835d2e7e05f90d081566a4ae85518
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-02-02 09:23:03 +0100

    raw sql

--- components/DashboardChart.tsx
+++ components/DashboardChart.tsx
@@ -1,21 +1,22 @@
 import { cached_students } from '@/prisma/db-calls';
 import { getNbStudentsByMonth } from '@/lib/utils';
 import { Card, LineChart, Title } from '@tremor/react';
+import prisma from '@/prisma/prismaClient';
 
-const months = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec',
-];
+const months = new Map([
+  [1, 'Jan'],
+  [2, 'Feb'],
+  [3, 'Mar'],
+  [4, 'Apr'],
+  [5, 'May'],
+  [6, 'June'],
+  [7, 'Jul'],
+  [8, 'Aug'],
+  [9, 'Sep'],
+  [10, 'Oct'],
+  [11, 'Nov'],
+  [12, 'Dec'],
+]);
 
 async function DashboardChart({
   students,
@@ -25,13 +26,27 @@ async function DashboardChart({
   // const isPro = session && subscription;
 
   // get number of students created in a specific month
-  const chartData = months.map((month, i) => ({
-    month,
-    students: getNbStudentsByMonth(students, i),
-    // ...(isPro && { teachers: getNbStudentsByMonth(teachers, i) }),
+  type QueryResult = {
+    month: number;
+    nb_students: number;
+  };
+  let result: QueryResult[] =
+    await prisma.$queryRaw`SELECT CAST(EXTRACT(MONTH FROM created_at) AS INTEGER) AS month, 
+                           CAST(COUNT(*) AS INTEGER) AS nb_students
+                           FROM "Student" 
+                           GROUP BY month
+                           ORDER BY month`;
+
+  type ChartData = {
+    month: string;
+    nb_students: number;
+  };
+  const chartData: ChartData[] = result.map((entry) => ({
+    month: months.get(entry.month)!,
+    nb_students: entry.nb_students,
   }));
 
-  const categories = ['students'];
+  const categories = ['nb_students'];
   // if (isPro) categories.push('teachers');
 
   return (
