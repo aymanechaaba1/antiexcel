30e851c8b41f597976eab92efad9a7e1e6ab28ea
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-02-02 09:35:38 +0100

    deleted home link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,19 +7,9 @@ import { usePathname } from 'next/navigation';
 type NavLink = {
   path: string;
   label: string;
-  searchParams?: URLSearchParams;
 };
 
-const searchParams = new URLSearchParams();
-searchParams.set('page', '1');
-searchParams.set('per_page', '5');
-searchParams.set('sort_by', 'latest');
-
 const navLinks: NavLink[] = [
-  {
-    path: '/',
-    label: 'Home',
-  },
   {
     path: '/dashboard',
     label: 'Dashboard',
@@ -31,12 +21,10 @@ const navLinks: NavLink[] = [
   {
     path: '/teachers',
     label: 'Teachers',
-    searchParams,
   },
   {
     path: '/contacts',
     label: 'Contacts',
-    searchParams,
   },
 ];
 
@@ -45,29 +33,19 @@ function Nav() {
 
   return (
     <div className="hidden md:flex items-center gap-3">
-      {navLinks.map((link, i) =>
-        link.searchParams ? (
-          <Link
-            key={i}
-            href={`${link.path}?${link.searchParams.toString()}`}
-            className={cn('text-sm text-gray-500', {
-              'text-blue-500 font-medium': pathname.startsWith(link.path),
-            })}
-          >
-            {link.label}
-          </Link>
-        ) : (
-          <Link
-            key={i}
-            href={link.path}
-            className={cn('text-sm text-gray-500', {
-              'text-blue-500 font-medium': pathname === link.path,
-            })}
-          >
-            {link.label}
-          </Link>
-        )
-      )}
+      {navLinks.map((link, i) => (
+        <Link
+          key={i}
+          href={link.path}
+          className={cn('text-sm text-gray-500', {
+            'text-blue-500 font-medium': pathname
+              .slice(1)
+              .includes(link.label.toLowerCase()),
+          })}
+        >
+          {link.label}
+        </Link>
+      ))}
     </div>
   );
 }
