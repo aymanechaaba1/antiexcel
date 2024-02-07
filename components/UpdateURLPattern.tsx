4a6a8bdd09769121b6bf9aa7de43e1473649b57e
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-02-07 13:25:07 +0100

    update url pattern

--- components/UpdateURLPattern.tsx
+++ components/UpdateURLPattern.tsx
@@ -8,24 +8,29 @@ function UpdateURLPattern({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
+
   const page = searchParams.get('page');
   const per_page = searchParams.get('per_page');
   const sort_by = searchParams.get('sort_by');
 
   useEffect(() => {
-    const searchParams = new URLSearchParams();
+    let newSearchParams: URLSearchParams;
+
+    if (page && per_page) newSearchParams = new URLSearchParams(searchParams);
 
     if (!page || !per_page) {
+      newSearchParams = new URLSearchParams();
       if (
         pathname === '/students' ||
         pathname === '/teachers' ||
         pathname === '/contacts'
       ) {
-        searchParams.set('page', DEFAULT_PAGE.toString());
-        searchParams.set('per_page', DEFAULT_PER_PAGE.toString());
+        newSearchParams.set('page', DEFAULT_PAGE.toString());
+        newSearchParams.set('per_page', DEFAULT_PER_PAGE.toString());
 
-        if (!sort_by) searchParams.set('sort_by', DEFAULT_SORT_BY);
-        router.replace(`${pathname}?${searchParams.toString()}`);
+        if (!sort_by) newSearchParams.set('sort_by', DEFAULT_SORT_BY);
+
+        router.replace(`${pathname}/?${newSearchParams.toString()}`);
       }
     }
   }, [pathname, page, per_page, sort_by]);
