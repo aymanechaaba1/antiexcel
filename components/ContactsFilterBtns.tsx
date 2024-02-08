327ec4b3da5e211da6c88a9efbd7a087b6205b86
Aymane Chaaba <aymanechaaba1@gmail.com>
2024-02-08 11:42:18 +0100

    filter-contacts

--- components/ContactsFilterBtns.tsx
+++ components/ContactsFilterBtns.tsx
@@ -3,22 +3,26 @@
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import ShadcnSelectComponent from './ShadcnSelectComponent';
+import ResetFiltersBtn from './ResetFiltersBtn';
 
 const relationships = ['mother', 'father', 'brother', 'sister'] as const;
 
 function ContactsFilterBtns() {
-  const [relationship, setRelationship] = useState<string>('');
-
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const router = useRouter();
 
+  const [relationship, setRelationship] = useState<string>(
+    searchParams.get('relationship')?.toString() || ''
+  );
+
   useEffect(() => {
-    const newSearchParams = new URLSearchParams(searchParams);
+    const params = new URLSearchParams(searchParams);
 
-    if (relationship) newSearchParams.set('relationship', relationship);
+    if (relationship) params.set('relationship', relationship);
+    else params.delete('relationship');
 
-    router.replace(`${pathname}/?${newSearchParams.toString()}`);
+    router.replace(`${pathname}/?${params.toString()}`);
   }, [relationship]);
 
   return (
@@ -26,6 +30,11 @@ function ContactsFilterBtns() {
       <p className="tracking-tight font-semibold text-gray-500 mb-3 text-right">
         Filter By
       </p>
+      <ResetFiltersBtn
+        onClick={() => {
+          setRelationship('');
+        }}
+      />
       <ShadcnSelectComponent
         onValueChange={setRelationship}
         value={relationship}
